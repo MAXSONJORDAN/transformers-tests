eec4545d5629dff6584a9c9b207312e62384ba51
Maxson Jordan <maxson.jordan@gmail.com>
2023-04-06 23:01:20 -0300

    fix importers

--- vicuna.py
+++ vicuna.py
@@ -1,19 +1,24 @@
 from flask import Flask, request, jsonify
-from transformers import AutoTokenizer, AutoModelWithLMHead
+from transformers import LlamaForCausalLM, LlamaTokenizer
 
 app = Flask(__name__)
 
-tokenizer = AutoTokenizer.from_pretrained("eachadea/vicuna-13b")
-model = AutoModelWithLMHead.from_pretrained("eachadea/vicuna-13b")
+model = "eachadea/vicuna-13b"
+
+tokenizer = LlamaTokenizer.from_pretrained("eachadea/vicuna-13b")
+model = LlamaForCausalLM.from_pretrained("eachadea/vicuna-13b")
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
     input_text = request.json['input_text']
     input_ids = tokenizer.encode(input_text, return_tensors='pt')
-    output = model.generate(input_ids, max_length=50, do_sample=True, temperature=0.8)
+    output = model.generate(input_ids, max_length=50,
+                            do_sample=True, temperature=0.8)
     output_text = tokenizer.decode(output[0], skip_special_tokens=True)
     response = {'output_text': output_text}
     return jsonify(response)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
